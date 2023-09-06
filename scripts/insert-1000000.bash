1998e5a8f0f1762449d59e90be6a60eb704aba4a
vinhyenvodoi98 <vinhyenvodoi98@gmail.com>
2023-09-06 22:05:53 +0900

    update dump file

--- scripts/insert-1000000.bash
+++ scripts/insert-1000000.bash
@@ -5,7 +5,7 @@ total_inserts=1000000
 for ((i = 1; i <= total_inserts; i++)); do
     key=$(uuidgen)
     value=$(uuidgen)
-    ttl=$((RANDOM % 36000000))
+    ttl=36000000000
 
     redis-cli SETEX "$key" "$ttl" "$value"
 done
